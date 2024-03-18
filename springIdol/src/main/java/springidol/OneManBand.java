a23173916bbf0568f9f04674f2315a19138e4a4e
Eunkyy <melon0802@naver.com>
2024-03-19 01:59:03 +0900

    :sparkles: add defalt code

---
+++ springIdol/src/main/java/springidol/OneManBand.java
@@ -0,0 +1,25 @@
+package springidol;
+
+import java.util.*;
+
+public class OneManBand implements Performer {
+	private Map<String, Instrument> instruments;
+
+	public OneManBand() {
+	}
+
+	public void setInstruments(Map<String, Instrument> instruments) {
+		this.instruments = instruments;
+	}
+
+	@Override
+	public void perform() throws PerformanceException {
+		for (Map.Entry<String, Instrument> entry : instruments.entrySet()) {
+			String key = entry.getKey();
+			Instrument instrument = entry.getValue();
+			System.out.println(key + " : " + instrument);
+			instrument.play();
+		}
+	}
+
+}
